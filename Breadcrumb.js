83691eee4b2609c48181ae5983f5724d99787c1c
nbkhope <ryumemaru@yahoo.com>
2021-10-30 22:46:34 -0400

    Improve Breadcumb component

--- Breadcrumb.js
+++ Breadcrumb.js
@@ -1,25 +1,41 @@
 import React from 'react';
 
+function BreadcrumbItem(props) {
+  if (props.divider) {
+    return (
+      <li className="breadcrumb__item breadcrumb__item--divider">/</li>
+    );
+  }
+
+  return (
+    <li className={`breadcrumb__item ${props.active ? 'breadcrumb__item--active' : ''}`} aria-current={props.active ? 'page' : undefined}>
+      {props.children}
+    </li>
+  );
+}
+
 function Breadcrumb(props) {
   const children = props.children && props.children.length ? props.children : [props.children];
 
   return (
     <nav className={props.className} aria-label="breadcrumb">
       <ol className="breadcrumb">
-        {props.loading && <li>...</li>}
-        {children.map((child, index) => {
-          const active = index === children.length - 1;
-          return (
-            <React.Fragment key={index}>
-              {index !== 0 && (
-                <li className="breadcrumb__item--divider">/</li>
-              )}
-              <li className={active ? 'breadcrumb__item--active' : ''} aria-current={active ? 'page' : undefined}>
-                {typeof child === 'function' ? child() : child}
-              </li>
-            </React.Fragment>
-          );
-        })}
+        {props.loading ? (
+          <li>...</li>
+        ) : children.reduce((list, child, index) => {
+          const items = [];
+
+          if (index !== 0) {
+            items.push(<BreadcrumbItem key={`divider-${index}`} divider />);
+          }
+          items.push(
+            <BreadcrumbItem key={index} active={index === children.length - 1}>
+              {typeof child === 'function' ? child() : child}
+            </BreadcrumbItem>
+          )
+
+          return list.concat(items);
+        }, [])}
       </ol>
     </nav>
   );
