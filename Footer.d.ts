e3b760bc002f03d1225ec00868022d4439266eca
nbkhope <ryumemaru@yahoo.com>
2021-11-06 00:37:19 -0400

    Add declaration file for Footer and Header

---
+++ Footer.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface FooterProps {
+  children: React.ReactNode;
+  className?: string;
+}
+declare function Footer(props: FooterProps): JSX.Element;
+export default Footer;
---
+++ Header.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+declare function Header(props: HeaderProps): JSX.Element;
+export default Header;
