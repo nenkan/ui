920c4f0dc8dfeaa59b38ad50c44cf7181a50fd83
nbkhope <ryumemaru@yahoo.com>
2021-12-28 19:03:54 -0500

    Add className prop to Notice

--- Notice.js
+++ Notice.js
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 
 export default function Notice(props) {
   return (
-    <div className={classNames('panel', { 'panel--danger': props.error })}>
+    <div className={classNames('panel', { 'panel--danger': props.error }, props.className)}>
       {props.message || props.children}
     </div>
   )
