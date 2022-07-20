eba2119b0081c75491213a213b32a94c1cfd3723
nbkhope <ryumemaru@yahoo.com>
2022-07-19 22:23:47 -0400

    Remove ErrorNotice (why? you can use Notice with modifier danger)

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 export { default as Breadcrumb } from './Breadcrumb';
 export { default as Container } from './Container';
-export { default as ErrorNotice } from './ErrorNotice';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as MarkdownRegion } from './MarkdownRegion';
