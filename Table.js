b81826a91c5ffab670b8cc88ba9662b88423a9ce
Nirav_Prajapati <niravprajapati97120@gmail.com>
2022-09-18 09:29:12 +0530

    Create Table.js

---
+++ Table.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export default function Table() {
+
+  return (
+    <table className = "table"> 
+
+        <tr> 
+
+          <th>Name</th> 
+
+          <th>Age</th> 
+
+          <th>Gender</th> 
+
+        </tr> 
+
+        <tr> 
+
+          <td>Anom</td> 
+
+          <td>19</td> 
+
+          <td>Male</td> 
+
+        </tr> 
+
+        <tr> 
+
+          <td>Megha</td> 
+
+          <td>19</td> 
+
+          <td>Female</td> 
+
+        </tr> 
+
+        <tr> 
+
+          <td>Subham</td> 
+
+          <td>25</td> 
+
+          <td>Male</td> 
+
+        </tr> 
+
+      </table>
+
+  );
+
+}
