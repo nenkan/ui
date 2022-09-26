d99cd56f64d7985feb9abe9df272166392a73e95
Zeddovar <89256287+Zeddovar@users.noreply.github.com>
2022-09-25 22:19:43 -0300

    Update panel.stories.jsx

--- stories/panel.stories.jsx
+++ stories/panel.stories.jsx
@@ -8,3 +8,7 @@ export default {
 export var panel_example = function() {
   return <Panel>Lorem ipsum</Panel>
 }
+
+export const PanelExampleDanger = function() {
+  return <Panel modifier="danger">Lorem ipsum danger</Panel>
+}
