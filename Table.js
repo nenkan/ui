b5a803db78d816c5fbe33cba0edd4f97a1caf3cb
Nirav_Prajapati <niravprajapati97120@gmail.com>
2022-09-26 20:00:29 +0530

    Update Table.js
    Co-authored-by: nbkhope <nbkhope@users.noreply.github.com>

--- Table.js
+++ Table.js
@@ -5,47 +5,7 @@ export default function Table(props) {
 
   return (
     <table className="table"> 
-
-        <tr> 
-
-          <th>{props.children}</th> 
-
-          <th>{props.children}</th> 
-
-          <th>{props.children}</th> 
-
-        </tr> 
-
-        <tr> 
-
-          <td>{props.children}</td> 
-
-          <td>{props.children}</td> 
-
-          <td>{props.children}</td> 
-
-        </tr> 
-
-        <tr> 
-
-          <td>{props.children}</td> 
-
-          <td>{props.children}</td> 
-
-          <td>{props.children}</td> 
-
-        </tr> 
-
-        <tr> 
-
-          <td>{props.children}</td> 
-
-          <td>{props.children}</td> 
-
-          <td>{props.children}</td> 
-
-        </tr> 
-
+      {props.children}
       </table>
 
   );
