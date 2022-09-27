ffd4c1d80a4905d7ce96a02e51b17dbe4ff8faed
Nirav_Prajapati <niravprajapati97120@gmail.com>
2022-09-27 13:46:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-export { default as Breadcrumb } from './Breadcrumb';
+export { default as Breadcrumb } from './breadcrumb';
 export { default as Container } from './Container';
 export { default as Footer } from './Footer';
-export { default as Header } from './Header';
-export { default as MarkdownRegion } from './MarkdownRegion';
+export { default as Header } from './header';
+export { default as MarkdownRegion } from './markdown-region';
 export { default as Modal } from './modal';
 export { default as Notice } from './Notice';
