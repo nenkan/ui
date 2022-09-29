1bfd73799896d9b8061f6c5e1076eec03a98551a
Auguxtow <58446352+Auguxtow@users.noreply.github.com>
2022-09-28 21:12:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export { default as Breadcrumb } from './Breadcrumb';
 export { default as Container } from './Container';
-export { default as Footer } from './Footer';
+export { default as Footer } from './footer';
 export { default as Header } from './Header';
 export { default as MarkdownRegion } from './MarkdownRegion';
 export { default as Modal } from './Modal';
