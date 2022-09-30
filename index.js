78ba4ea742dfdfd77035db7348515aeb00a07d03
Auguxtow <58446352+Auguxtow@users.noreply.github.com>
2022-09-30 20:57:40 -0300

    Added lowercase

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-export { default as Breadcrumb } from './Breadcrumb';
+export { default as Breadcrumb } from './breadcrumb';
 export { default as Container } from './container';
-export { default as Footer } from './Footer';
+export { default as Footer } from './footer';
 export { default as Header } from './header';
 export { default as MarkdownRegion } from './markdown-region';
 export { default as Modal } from './modal';
-export { default as Notice } from './Notice';
+export { default as Notice } from './notice';
