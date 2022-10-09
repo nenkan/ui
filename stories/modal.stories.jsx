2f6f1dc013e07610d9808602a3a2bc7c3892b6dd
nbkhope <ryumemaru@yahoo.com>
2022-10-08 23:22:55 -0400

    Add story for Modal

---
+++ stories/modal.stories.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '@nenkan/css/modal.scss';
+
+import { Modal } from "..";
+
+export default {
+  title: 'Modal'
+};
+
+export const ModalExample = () => {
+  const [hidden, setHidden] = React.useState(false);
+
+  if (hidden) {
+    return null;
+  }
+
+  return (
+    <Modal onCancel={() => setHidden(true)} title="Lorem Ipsum">
+      <div>
+        This is the modal content.
+      </div>
+    </Modal>
+  );
+};
