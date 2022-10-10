211a5bfc48b2bc75ed8637176c6b2b0921dc759f
Zeddovar <89256287+Zeddovar@users.noreply.github.com>
2022-10-09 21:31:37 -0300

    Update table.stories.jsx

--- stories/table.stories.jsx
+++ stories/table.stories.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import {Table} from  '..'
+import React from 'react';
+import {Table} from  '..';
 
 export default {
- title: 'Table'
-}
+  title: 'Table'
+};
 
 export function TableExample() {
-   return (
-     <Table>
-        <thead>
-          <tr>
-            <th>Lorem</th>
-            <th>Ipsum</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Dolor</td>
-            <td>000</td>
-          </tr>
-          <tr>
-            <td>Amet</td>
-            <td>000</td>
-          </tr>
-          <tr>
-            <td>Consectetur</td>
-            <td>000</td>
-          </tr>
-        </tbody>
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Lorem</th>
+          <th>Ipsum</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Dolor</td>
+          <td>000</td>
+        </tr>
+        <tr>
+          <td>Amet</td>
+          <td>000</td>
+        </tr>
+        <tr>
+          <td>Consectetur</td>
+          <td>000</td>
+        </tr>
+      </tbody>
     </Table>
-   )
+   );
 }
