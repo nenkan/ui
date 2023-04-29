d446aaa99c59d3e66f0404eff17765b6d8d6bf2a
nbkhope <ryumemaru@yahoo.com>
2023-04-29 00:14:49 -0400

    Upgade storybook to version 7

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,47 @@
 const os = require('os');
 
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended'
-    ],
-    'ignorePatterns': [
-        '.eslintrc.js',
-        'babel.config.js',
-    ],
-    'overrides': [
-    ],
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            os.EOL === '\r\n' ? 'windows' : 'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        "react/prop-types": [
-            0
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:storybook/recommended'
+  ],
+  'ignorePatterns': [
+    '.eslintrc.js',
+    'babel.config.js',
+  ],
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+      'indent': [
+          'error',
+           2
+      ],
+      'linebreak-style': [
+        'error',
+        os.EOL === '\r\n' ? 'windows' : 'unix'
+      ],
+      'quotes': [
+        'error',
+        'single'
+      ],
+      "react/prop-types": [
+        0
+      ],
+      'semi': [
+        'error',
+        'always'
+      ]
+  }
 };
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,27 +1,27 @@
 module.exports = {
-  core: {
-    builder: 'webpack5'
+  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", '@storybook/preset-scss', "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    '@storybook/preset-scss'
-  ],
-  "framework": "@storybook/react",
-  webpackFinal(config, { configType }) {
+  webpackFinal(config, {
+    configType
+  }) {
     if (configType === 'PRODUCTION') {
-      config.output.publicPath = '/ui/'
+      config.output.publicPath = '/ui/';
     }
     return config;
   },
-  managerWebpack(config, { configType }) {
+  managerWebpack(config, {
+    configType
+  }) {
     if (configType === 'PRODUCTION') {
-      config.output.publicPath = '/ui/'
+      config.output.publicPath = '/ui/';
     }
     return config;
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
