71a2b09c94cf95741faa1343aa31d5ce67278e1b
nbkhope <ryumemaru@yahoo.com>
2024-05-03 22:58:29 -0400

    Add modifier warning to Notice, showIcon prop

--- src/notice.js
+++ src/notice.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import { GoAlert } from 'react-icons/go';
 
 function Notice(props) {
   const [closed, setClosed] = useState(false);
@@ -16,7 +17,8 @@ function Notice(props) {
 
   //todo: warning icon
   return (
-    <div className={classNames('notice', { 'notice--danger': props.modifier === 'danger', 'notice--floating': props.floating }, props.className)}>
+    <div className={classNames('notice', { 'notice--danger': props.modifier === 'danger', 'notice--warning': props.modifier === 'warning', 'notice--floating': props.floating }, props.className)}>
+      {props.showIcon && <GoAlert className="notice__icon" />}
       <div className="notice__message">{props.children}</div>
       {(props.dismissable == null ? props.floating : props.dismissable) && (
         <button className="notice__close-button" type="button" onClick={onCloseClick} aria-label="close notice" title="close notice">X</button>
@@ -30,8 +32,9 @@ Notice.propTypes = {
   className: PropTypes.string,
   dismissable: PropTypes.bool,
   floating: PropTypes.bool,
-  modifier: PropTypes.oneOf(['danger']),
+  modifier: PropTypes.oneOf(['danger', 'warning']),
   onDismiss: PropTypes.func,
+  showIcon: PropTypes.bool,
 };
 
 Notice.defaultProps = {
