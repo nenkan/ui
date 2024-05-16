d9533bffc4e63d2be707ce78540772f9c0e70ae8
aditya-raj2230 <aditya17025@gmail.com>
2024-05-16 10:39:56 +0530

    #105 issue resolved
    We can pass all the props of button to Button component

--- src/button.jsx
+++ src/button.jsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 
 export default function Button(props) {
   return (
-    <button className={classNames('button', props.className)}>
+    <button {...props}>
       {props.children}
     </button>
   );
