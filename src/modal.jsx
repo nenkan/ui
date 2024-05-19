81db107fbdd381b07528f41e08ac1c314c59d915
nbkhope <ryumemaru@yahoo.com>
2024-05-18 20:49:44 -0400

    Convert Modal to functional component instead of class

--- src/modal.jsx
+++ src/modal.jsx
@@ -1,31 +1,23 @@
 import React from 'react';
 
-class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.onCancelClick = this.onCancelClick.bind(this);
+function Modal(props) {
+  function onCancelClick() {
+    props.onCancel();
   }
 
-  onCancelClick() {
-    this.props.onCancel();
-  }
-
-  render() {
-    return (
-      <div className="modal">
-        <div className="modal__container">
-          <header className="modal__header">
-            {this.props.title && (
-              <div className="modal__title">{this.props.title}</div>
-            )}
-            <button className="button button--icon modal__close-button" type="button" onClick={this.onCancelClick}>x</button>
-          </header>
-          {this.props.children}
-        </div>
+  return (
+    <div className="modal">
+      <div className="modal__container">
+        <header className="modal__header">
+          {props.title && (
+            <div className="modal__title">{props.title}</div>
+          )}
+          <button className="button button--icon modal__close-button" type="button" onClick={onCancelClick}>x</button>
+        </header>
+        {props.children}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Modal;
