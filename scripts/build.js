80b16d84e7f1a4c0a2ce64e4c422e9dce573ffd1
nbkhope <ryumemaru@yahoo.com>
2024-09-03 12:52:41 -0400

    Update package.json main field from dist/index.js to index.js when you build

--- scripts/build.js
+++ scripts/build.js
@@ -28,8 +28,18 @@ async function copyOtherFiles() {
   }
 }
 
+// Replace "main": "dist/index.js" with "main": "index.js"
+async function updatePackageJsonMain() {
+  const packageJsonPath = path.join(buildDirectory, 'package.json');
+  const packageJsonString = await fs.promises.readFile(packageJsonPath, { encoding: 'utf-8' });
+  const packageJson = JSON.parse(packageJsonString);
+  packageJson.main = packageJson.main.replace(/dist\//, '');
+  await fs.promises.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), { encoding: 'utf-8' });
+}
+
 async function build() {
   await copyOtherFiles();
+  await updatePackageJsonMain();
 }
 
 build();
