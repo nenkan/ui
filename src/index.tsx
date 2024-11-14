74b23478e03382f50ab4ebf01b997969d10dae62
nbkhope <ryumemaru@yahoo.com>
2024-11-14 01:00:03 -0500

    Rename file extensions from jsx to tsx

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+export { default as Breadcrumb } from './breadcrumb.js';
+export { default as Button } from './button.js';
+export { default as Container } from './container.js';
+export { default as Footer } from './footer.js';
+export { default as Header } from './header.js';
+export { default as MarkdownRegion } from './markdown-region.js';
+export { default as Modal } from './modal.js';
+export { default as Notice } from './notice.js';
+export { default as Panel } from './panel.js';
+export { default as Table } from './table.js';
