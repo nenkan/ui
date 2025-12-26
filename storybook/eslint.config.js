a56a9ac0922b21986a9033ba6d084b86ed518210
nbkhope <ryumemaru@yahoo.com>
2025-12-25 22:38:24 -0500

    Ignore storybook-static directory when linting

--- storybook/eslint.config.js
+++ storybook/eslint.config.js
@@ -9,7 +9,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'storybook-static']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
